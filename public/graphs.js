d7268744a31dfba8bb58e4a9770088f5afe9bcc8
NixGD <nixgdill@gmail.com>
2017-04-29 13:06:06 -0400

    Made graph code resuable, produced other graphs

--- public/graphs.js
+++ public/graphs.js
@@ -1,8 +1,8 @@
 /**
  * Created by nix on 4/29/17.
  */
-window.onload = function () {
-    var svg = d3.select("svg"),
+function create_graph(yKey, yLabel) {
+    var svg = d3.select("#" + yKey),
         margin = {top: 20, right: 20, bottom: 50, left: 100},
         width = +svg.attr("width") - margin.left - margin.right,
         height = +svg.attr("height") - margin.top - margin.bottom,
@@ -21,21 +21,21 @@ window.onload = function () {
 
     var line = d3.line()
         .x(function(d) { return x(parseTime(d)); })
-        .y(function(d) { return y(d.kwh); });
+        .y(function(d) { return y(d[yKey]); });
 
     d3.json("data.json",
         function(error, data) {
             if (error) throw error;
 
             x.domain(d3.extent(data, function(d) { return parseTime(d); }));
-            y.domain([0, d3.max(data, function(d) { return d.kwh; })]);
+            y.domain([0, d3.max(data, function(d) { return d[yKey]; })]);
 
             g.append("g")
                 .attr("transform", "translate(0," + height + ")")
                 .call(
                     d3.axisBottom(x)
                         .ticks(d3.timeMonth.every(1))
-                    )
+                )
                 .select(".domain")
                 .remove();
 
@@ -67,7 +67,14 @@ window.onload = function () {
                 .attr("x", 0 - (height / 2) - margin.top)
                 .attr("dy", "1em")
                 .style("text-anchor", "middle")
-                .text("Energy  (kWh)");
+                .text(yLabel);
+        }
+    );
+}
 
-        });
+
+window.onload = function () {
+    create_graph("kwh", "Energy (kWh)");
+    create_graph("bill", "Bill ($)");
+    create_graph("savings", "Savings ($)");
 };
